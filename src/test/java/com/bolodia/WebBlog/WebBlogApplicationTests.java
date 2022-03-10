636bda3f0b195174bf0af3654111b0e1e1aaf092
VovaDemidov <vdemidov1987@gmail.com>
2022-03-10 15:42:36 +0300

    Init

---
+++ src/test/java/com/bolodia/WebBlog/WebBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bolodia.WebBlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
