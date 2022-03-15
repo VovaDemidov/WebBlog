bb0db02f2840391886a92ca0d4eaf6a027dcfeb5
VovaDemidov <vdemidov1987@gmail.com>
2022-03-15 22:23:53 +0300

    V2

--- src/main/java/com/bolodia/WebBlog/controllers/BlogController.java
+++ src/main/java/com/bolodia/WebBlog/controllers/BlogController.java
@@ -7,8 +7,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.Optional;
 
 @Controller
 public class BlogController {
@@ -22,7 +26,7 @@ public class BlogController {
     public String blogMain(Model model) {
 
 
-         Iterable < Post > posts = postInterface.findAll ( );
+        Iterable < Post > posts = postInterface.findAll ( );
 
         model.addAttribute ("posts", posts);
         return "blogMain";
@@ -46,5 +50,65 @@ public class BlogController {
 
     }
 
+    @GetMapping("/blog/{id}")
+    public String blogFullText(@PathVariable(value = "id") long id, Model model) {
+
+        if (postInterface.existsById (id)) {
+
+            Optional < Post > post = postInterface.findById (id);
+            ArrayList < Post > res = new ArrayList <> ( );
+            post.ifPresent (res::add);
+            model.addAttribute ("posts", res);
+            return "blogDetails";
+        }
+
+        return "redirect:/blog";
+
+    }
+
+    @GetMapping("/blog/{id}/edit")
+    public String blogEdit(@PathVariable(value = "id") long id, Model model) {
+
+        if (postInterface.existsById (id)) {
+
+            Optional < Post > post = postInterface.findById (id);
+            ArrayList < Post > res = new ArrayList <> ( );
+            post.ifPresent (res::add);
+            model.addAttribute ("posts", res);
+            return "blogEdit";
+        }
+
+        return "redirect:/blog";
+
+
+    }
+
+
+    @PostMapping("/blog/{id}/edit")
+    public String blogPostUpdate(@PathVariable(value = "id") long id, @RequestParam String title, @RequestParam String author, @RequestParam String shortText, @RequestParam String fullText, Model model) {
+        Post post = postInterface.findById (id)
+                .orElseThrow ( );
+
+        post.setTitle (title);
+        post.setAuthor (author);
+        post.setShortText (shortText);
+        post.setFullText (fullText);
+
+        postInterface.save (post);
+
+        return "redirect:/blog";
+
+    }
+
+
+    @PostMapping("/blog/{id}/remove")
+    public String blogPostRemove(@PathVariable(value = "id") long id, Model model) {
+        Post post = postInterface.findById (id)
+                .orElseThrow ( );
+       postInterface.deleteById (id);
+
+        return "redirect:/blog";
+
+    }
 
 }
